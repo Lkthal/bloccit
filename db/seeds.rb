28ee0ebc56660856695848219b855cbed196e6a3
Thal Syhabout <tsyhabout@gmail.com>
2018-05-01 01:57:17 -0700

    complete populating seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+#require 'random_data'
+# create Post
+50.times do
+
+  Post.create!(
+    title: RandomData.random_sentence,
+    body: RandomData.random_paragraph
+  )
+end
+posts = Post.all
+
+#create comments
+100.times do
+  Comment.create!(
+    post: posts.sample,
+    body: RandomData.random_paragraph
+  )
+end
+puts "#{Post.count}"
+Post.find_or_create_by(title: "A unique title", body: "A unique body")
+puts "#{Post.count}"
+puts "#{Comment.count}"
+Comment.find_or_create_by(body: "unique body")
+puts "#{Comment.count}"
+
+puts "Seed finished"
+puts "#{Post.count} post created"
+puts "#{Comment.count} comments created"
