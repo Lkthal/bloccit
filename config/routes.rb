0a3443cae6aac73fe16d3951dd0c064609c251f8
Thal Syhabout <tsyhabout@gmail.com>
2018-05-06 02:27:18 -0700

    added user confirmation

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   end
 
   resources :users, only: [:new, :create]
-  
+  post 'user/confirm' => 'users#confirm'
   get 'about' => 'welcome#about'
   get 'contact'=>'welcome#contact'
   get 'faq'=>'welcome#faq'
