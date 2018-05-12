d0377a333db80bc1a37138bc92f097b0f8e38384
Thal Syhabout <tsyhabout@gmail.com>
2018-05-11 20:21:59 -0700

    finish favorite assignment

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -1,15 +1,29 @@
 class FavoriteMailer < ApplicationMailer
-  default from: "youremail@email.com"
+  default from: "tsyhabout@yahoo.com"
 
-  def new_comment(user,post,comment)
+
+  def new_comment(user, post, comment)
+
+
+     headers["Message-ID"] = "<comments/#{comment.id}@your-app-name.example>"
+     headers["In-Reply-To"] = "<post/#{post.id}@your-app-name.example>"
+     headers["References"] = "<post/#{post.id}@your-app-name.example>"
+
+     @user = user
+     @post = post
+     @comment = comment
+
+     mail(to: user.email, subject: "New comment on #{post.title}")
+   end
+
+  def new_post(post)
     headers["Message-ID"] = "<comments/#{comment.id}@your-app-name.example>"
     headers["In-Reply_To"] = "<post/#{post.id}@your-app-name.example>"
     headers["References"] = "<post/#{post.id}@your-app-name.example>"
 
-    @user = user
     @post = post
-    @comment = comment
 
-    mail(to: user.email, subject: "New comment on #{post.title}")
+    mail(to: post.user.email, subject: "You are now following your new post: #{post.title}")
   end
+
 end
